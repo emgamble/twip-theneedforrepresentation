4bc19020c35290f321e3dcc9d22916d928ed80f2
eg913 <eg913@pvsd.org>
2018-09-24 10:55:39 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner wordReader = new Scanner(System.in);
+    System.out.println("Enter Word Here: ");
+    String a = wordReader.nextLine();
+    int c = a.length();
+
+
+
+    }
+}
