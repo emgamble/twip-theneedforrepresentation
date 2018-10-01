c136eb86300764c147b5e2e52937bcb4b897967b
emgamble <eg913@pvsd.org>
2018-09-30 21:30:28 -0400

    not even remotely done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
@@ -9,7 +10,52 @@ public class Main {
     Scanner wordReader = new Scanner(System.in);
     System.out.println("Enter Word Here: ");
     String a = wordReader.nextLine();
+    a.toUpperCase();
     int c = a.length();
+
+
+
+    Random startRandom = new Random();
+    int r = startRandom.nextInt(5)+1;
+    //System.out.println(r);
+
+    double p = Math.random();
+    int s = (int) p * c;
+
+
+
+
+
+
+
+
+
+
+
+    //if (r == 1)
+        //binary
+        //for(int j=0; j<= c; j++)
+            //s = s + 1;
+            //count ++;
+
+
+
+
+   // else if (r == 2)
+        //octal
+    //else if ( r == 3)
+        //decimal
+   // else if (r == 4)
+        //hex
+    //else
+        //ASCII
+
+
+
+
+
+
+
 
 
 
