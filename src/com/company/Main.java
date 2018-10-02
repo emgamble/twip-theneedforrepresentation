0245b7302e365113e43fbc40c4bcc39ad203b692
emgamble <eg913@pvsd.org>
2018-10-01 21:51:41 -0400

    not done but made a lot of progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,44 +10,74 @@ public class Main {
     Scanner wordReader = new Scanner(System.in);
     System.out.println("Enter Word Here: ");
     String a = wordReader.nextLine();
-    a.toUpperCase();
-    int c = a.length();
+
+    int wordLength = a.length();
+   // int ascii = a.charAt(0);
+   // System.out.println(ascii);
+
+
 
 
 
     Random startRandom = new Random();
-    int r = startRandom.nextInt(5)+1;
-    //System.out.println(r);
+    int randm = startRandom.nextInt(5);
+    System.out.println(randm);
+    double percent = Math.random();
+    int start = (int) percent * wordLength;
+    System.out.println(start);
 
-    double p = Math.random();
-    int s = (int) p * c;
-
-
-
-
+    int count = 0;
 
 
+    while (count < wordLength) {
+        int AsciiValue = a.charAt(start);
+        System.out.println(AsciiValue);
+        count++;
+        start++;
+        randm++;
+        if (start > wordLength) {
+            start = start - wordLength;
+        }
+
+
+        if (randm > 5) {
+            randm = randm - 5;
+        }
+
+
+        if (randm == 1)
+           System.out.print(Integer.toBinaryString(AsciiValue - 64));
+        else if (randm == 2)
+            System.out.print(Integer.toOctalString(AsciiValue-64));
+        else if (randm == 3)
+            System.out.print(AsciiValue);
+        else if (randm == 4)
+            System.out.print(Integer.toHexString(AsciiValue-64));
+        else if (randm == 5)
+            System.out.print(a.charAt(start));
+
+    }
 
 
 
 
 
-    //if (r == 1)
+       // if (random == 1)
         //binary
-        //for(int j=0; j<= c; j++)
-            //s = s + 1;
-            //count ++;
+            //for(int j=0; j<= wordLength; j++)
+                //start = start + 1;
 
 
 
 
-   // else if (r == 2)
+
+        //else if (random == 2)
         //octal
-    //else if ( r == 3)
+        //else if (random == 3)
         //decimal
-   // else if (r == 4)
+        //else if (random == 4)
         //hex
-    //else
+       // else
         //ASCII
 
 
